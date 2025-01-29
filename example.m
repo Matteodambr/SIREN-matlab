e0940808a888e9e9d9af4dbcc22018d51e91f7be
Matteodambr <96341031+Matteodambr@users.noreply.github.com>
2025-01-29 17:05:48 +0100

    Added example script for SIREN function calls

--- example.m
+++ example.m
@@ -1,14 +1,11 @@
 % Script used an example for function calls to generate SIREN network
 
+addpath(genpath('src')) ;
 
-
-
-
-
-
-
-
-
-
-
-
+% Example function call:
+neuronNumber = 200 ;
+actionNumber = 7 ;
+observationNumber = 30 ;
+omega_0 = 30 ; % Typical value from original paper that seems to work well for most applications
+actorNet = SIREN_Lx3_NxXXX_stochastic(neuronNumber,actionNumber, observationNumber, omega_0) ;
+analyzeNetwork(actorNet) ;
